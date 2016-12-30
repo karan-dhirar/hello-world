5c38cf7d6337f56ed7a1080b187822e794ac5a8d
Karan <Karan@KARAN-PC>
2016-12-30 14:03:11 -0500

    STMSYS-1 lets see if it worked!

--- HelloWorld/src/main/java/sample/Hello.java
+++ HelloWorld/src/main/java/sample/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
-		System.out.println("adding another line!");
+		System.out.println("adding anothers line!");
 	}//removed junk!vvv
 }
