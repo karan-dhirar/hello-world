695d338c029fc1311b4a473e06d81befb1969bc8
Karan <Karan@KARAN-PC>
2017-01-02 21:10:21 -0500

    test3

--- HelloWorld/src/main/java/sample/Hello.java
+++ HelloWorld/src/main/java/sample/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+		System.out.println("hellos");
 		System.out.println("testing jenkins");
 		System.out.println("adding anothers line!");
 	}//removed junk!vvv us
